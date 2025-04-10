309252765692531528b5487cab45c651a79be9bf
Joe Lizardo Diaz Echevarria <joediaz88@hotmail.com>
2025-04-10 21:23:12 +0000

    configurando AboutPage

--- web/src/pages/AboutPage.jsx
+++ web/src/pages/AboutPage.jsx
@@ -1,9 +1,22 @@
-import { Typography } from "@mui/material";
+import { Box, Card, Container, Typography } from "@mui/material";
+import ResponsiveAppBar from "../components/Appbar";
+import CardItem from "../components/CardItem";
+import propic from "../assets/about/profilepic.jpg"
 
-export default function AboutPage (){
-    return(
+export default function AboutPage() {
+    return (
         <>
-        <Typography variant="h1">About</Typography>
+            <ResponsiveAppBar></ResponsiveAppBar>
+            <Container maxWidth="lg">
+                <Box sx={{backgroundColor:'red'}}>
+                    <Card>
+                        <CardItem image={propic}/>
+                    </Card>
+                    <Typography variant="h3">About Lara:</Typography>
+                    <Typography>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore libero et neque delectus harum beatae ab, accusantium voluptatum inventore iusto, quia incidunt ipsam voluptatem illo! Quis quasi harum aspernatur in!</Typography>
+                </Box>
+            </Container>
+
         </>
     )
 }
