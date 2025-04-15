f3eaf4d9739970a8b1eac13a76165cf7280bbbab
Joe Lizardo Diaz Echevarria <joediaz88@hotmail.com>
2025-04-15 18:51:49 +0000

    grid gallery, tamaño imagenes y tamaño blog arreglados

--- web/src/components/Gallery.jsx
+++ web/src/components/Gallery.jsx
@@ -1,6 +1,7 @@
 import { CardContent, CardHeader, Grid, ListItem, Typography } from "@mui/material"
 import CardItem from "./CardItem"
 import zeus from "../assets/about/ejemplo_zeus.jpg"
+import polución from "../assets/about/polucion.jpg"
 
 const arr = [
     {
@@ -12,7 +13,7 @@ const arr = [
         "titulo": 'Siglo XIX'
     },
     {
-        "image": 'https://placehold.co/600x300/png',
+        "image": polución,
         "titulo": 'Raper & Trapers'
     },
     {
@@ -23,10 +24,10 @@ const arr = [
 
 export default function Gallery() {
     return (
-        <Grid container spacing={0.5}>
+        <Grid container spacing={0.5} sx={{margin:3, padding:3}}>
             {arr.map((element, index) => {
                 return (
-                    <Grid key={index} size={{ xs: 12, md: 6, lg: 4 }}>
+                    <Grid key={index} size={{ xs: 12, md: 6, lg:6 }} sx={{mb:3}}>
                         <CardItem image={element.image} titulo={element.titulo} color={'blancoPerla.main'}></CardItem>
                     </Grid>
                 )
--- web/src/components/blog/BlogGrid.jsx
+++ web/src/components/blog/BlogGrid.jsx
@@ -7,12 +7,12 @@ export default function BlogGrid() {
     const blog = [
         {
             "fecha": "01-01-2025",
-            "titulo": "Titulo prueba que pueda ser bastante largo",
+            "titulo": "Titulo max 30 characteres y asd",
             "texto": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu."
         },
         {
             "fecha": "02-01-2025",
-            "titulo": "Titulo prueba que pueda ser bastante largo",
+            "titulo": "Titulo max 30 characteres y asd",
             "texto": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem."
         },
         {
@@ -44,25 +44,24 @@ export default function BlogGrid() {
 
     return (
         <>
-            <Box maxWidth={'lg'} height={{ xs: '425px', lg: '700px' }} sx={{ margin: 'auto' }}>
+            <Box maxWidth={'lg'} height={{ lg: '700px' }} sx={{ margin: 'auto' }}>
                 <Grid container spacing={3} height={'100%'}>
-                    <Grid size={{ xs: 12, md: 7 }} height={'100%'}>
-                        <Box height={'100%'}>
-                            <BlogFirstItem
-                                image={'https://cdn.pixabay.com/photo/2016/12/15/20/21/texture-1909992_640.jpg'}
-                                titulo={blogElement1.titulo}
-                                texto={blogElement1.texto}
-                                fecha={blogElement1.fecha}
-                            >
-                            </BlogFirstItem>
-                        </Box>
+                    <Grid size={{ xs: 12, md: 12, lg: 8 }} height={{ lg: '700px' }}>
+                        <BlogFirstItem
+                            image={'https://cdn.pixabay.com/photo/2016/12/15/20/21/texture-1909992_640.jpg'}
+                            titulo={blogElement1.titulo}
+                            texto={blogElement1.texto}
+                            fecha={blogElement1.fecha}
+                        >
+                        </BlogFirstItem>
                     </Grid>
-                    <Grid size={{ xs: 12, md: 5 }} height={'100%'}>
+                    <Grid size={{ xs: 12, md: 12, lg: 4 }} height={{ lg: '700px' }}>
                         <Stack spacing={2} height={'100%'}>
                             {slicedBlog.map((blog, index) => {
                                 return (
                                     <>
                                         <BlogSmallItem
+                                            key={index}
                                             image={'https://cdn.pixabay.com/photo/2016/12/15/20/21/texture-1909992_640.jpg'}
                                             bgColor={'secondary.main'}
                                             titulo={blog.titulo}
