5777aa1bf1b8ae504d9e66011a84f7e75b527c1e
Joe Lizardo Diaz Echevarria <joediaz88@hotmail.com>
2025-04-20 20:40:10 +0000

    agregados elementos a ProductPage, textfield y texto para descripción

--- web/src/pages/ProductPage.jsx
+++ web/src/pages/ProductPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Grid, TextField, Typography } from "@mui/material";
 import ResponsiveAppBar from "../components/Appbar";
 import { useParams } from "react-router";
 import ShoppingCart from "../components/store/ShoppingCart";
@@ -9,18 +9,50 @@ export default function () {
     return (
         <>
             <ResponsiveAppBar></ResponsiveAppBar>
-            <Typography variant="h4" color='secondary' sx={{ textAlign: 'center', mb: 4 }}>Shop</Typography>
+            <Typography variant="h4" color='secondary' sx={{ textAlign: 'center', mb: 3 }}>Shop</Typography>
             <Box maxWidth={'lg'} sx={{ margin: 'auto' }}>
                 <Box container sx={{ display: 'flex', marginTop: 6, padding: 6 }}>
                     <Box >
                         <Box
                             component='img'
-                            src='https://placehold.co/600x600/png'
+                            src='https://placehold.co/500x400/png'
                         />
                     </Box>
-                    <Box sx={{marginLeft:5}}>
-                        <Typography variant="h3" color="tertiary">{mayusc}</Typography>
-                        <Typography variant="h6">Precio</Typography>
+                    <Box sx={{ marginLeft: 5 }}>
+                        <Box sx={{ marginTop: 1 }}>
+                            <Typography variant="h4" color="tertiary">{mayusc}</Typography>
+                        </Box>
+                        <Box sx={{ marginTop: 3 }}>
+                            <Typography sx={{ fontSize: '18px' }}>€ 60.00</Typography>
+                        </Box>
+                        <Box sx={{ marginTop: 3 }}>
+                            <Typography variant='h5'>Descripción</Typography>
+                        </Box>
+                        <Box sx={{ marginTop: 3 }}>
+                            <Typography variant='h5'>Otra información útil</Typography>
+                        </Box>
+                        <Box component="form"
+                            sx={{ '& .MuiTextField-root': { width: '15ch' } }}
+                            noValidate
+                            autoComplete="on">
+                            <TextField
+                                margin="dense"
+                                id="outlined-number"
+                                onChange={(event) =>
+                                    event.target.value < 1
+                                        ? (event.target.value = 1)
+                                        : event.target.value
+                                }
+                                type="number"
+                                size="small"
+                                defaultValue={1}
+                                slotProps={{
+                                    inputLabel: {
+                                        shrink: true,
+                                    },
+                                }}
+                            />
+                        </Box>
                     </Box>
                 </Box>
             </Box>
