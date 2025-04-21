fda0fe6d2c5ca05dad169f5d60ec9617f52003cd
Joe Lizardo Diaz Echevarria <joediaz88@hotmail.com>
2025-04-21 15:31:09 +0000

    descargado corsheaders para django, declaradas vista de lista producto y detalle producto,actualizado modelo producto con slug

--- api/api/settings.py
+++ api/api/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'users',
     'blog',
-    'shop'
+    'shop',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware"
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "api.urls"
 
