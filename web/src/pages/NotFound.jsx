5bd0b416e71ec3c3cca74606bd08b402934e41b6
JoEDE88 <joediaz88@hotmail.com>
2025-05-23 23:03:43 +0200

    pagina not found mejorada

--- web/src/pages/NotFound.jsx
+++ web/src/pages/NotFound.jsx
@@ -1,12 +1,12 @@
 import { Box, Typography } from "@mui/material";
 import ResponsiveAppBar from "../components/layout/Appbar";
 import Footer from "../components/layout/Footer";
+import Layout from "../components/layout/Layout";
 
 export default function NotFound (){
     return (
         <>
-        <Box sx={{height:'78vh'}}>
-        <ResponsiveAppBar></ResponsiveAppBar>
+        <Layout>
         <Box sx={{display:'flex',margin:'auto',flexDirection:'column'}}>
         <Box sx={{display:'flex', justifyContent:'center', marginBottom:5, marginTop:5}}>
         <Typography variant="h4">404 - Página no encontrada</Typography>
@@ -15,8 +15,7 @@ export default function NotFound (){
         <Typography variant="p">La ruta que estás buscando no existe.</Typography>
         </Box>
         </Box>
-        </Box>
-        <Footer></Footer>
+        </Layout>
         </>
     )
 }
