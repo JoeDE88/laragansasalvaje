3052fd52b5db7d3378742132fe115a7525331c4b
Joe Lizardo Diaz Echevarria <joediaz88@hotmail.com>
2025-05-23 22:39:26 +0000

    preparando deploy

--- django_back/django_back/settings.py
+++ django_back/django_back/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2)_ygc#um)zhe)yy^6z09hvx5lni7650l079il2bio4b-&_*n@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 
 ALLOWED_HOSTS = ['.localhost','127.0.0.1']
