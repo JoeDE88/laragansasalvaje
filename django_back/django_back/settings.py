3bae97a3f257fcf742e7c662dac8347cde887486
Joe Lizardo Diaz Echevarria <joediaz88@hotmail.com>
2025-05-24 01:20:28 +0200

    Update settings.py

--- django_back/django_back/settings.py
+++ django_back/django_back/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2)_ygc#um)zhe)yy^6z09hvx5lni7650l079il2bio4b-&_*n@"
+SECRET_KEY = os.getenv("SECRET_KEY","clave_por_defecto_para_dev")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
