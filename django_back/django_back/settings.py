56cd9c3e3c90eb7f14997e932186efa9cc1cbf2d
JoEDE88 <joediaz88@hotmail.com>
2025-05-28 20:03:13 +0200

    instalados paquetes para postgre

--- django_back/django_back/settings.py
+++ django_back/django_back/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -91,12 +92,8 @@ WSGI_APPLICATION = "django_back.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
