bd000a10554949355d2a99757bd01512fb592e1d
JoEDE88 <joediaz88@hotmail.com>
2025-05-29 19:58:46 +0200

    quitados permisos diferentes para usuarios

--- django_back/users/admin.py
+++ django_back/users/admin.py
@@ -1,34 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 from .models import Perfil
 
 # Register your models here.
-
-class ReadOnlyUserAdmin(DefaultUserAdmin):
-    add_fieldsets = DefaultUserAdmin.add_fieldsets
-
-    def has_add_permission(self, request):
-        return request.user.is_superuser
-    
-    def has_change_permission(self, request, obj = None):
-        return request.user.is_superuser
-    
-    def has_delete_permission(self, request, obj = None):
-        return False
-    
-
-admin.site.unregister(User)
-admin.site.register(User,ReadOnlyUserAdmin)
-
-class ReadOnlyPerfilAdmin(admin.ModelAdmin):
-    def has_add_permission(self, request):
-        return request.user.is_superuser
-
-    def has_change_permission(self, request, obj=None):
-        return request.user.is_superuser
-
-    def has_delete_permission(self, request, obj=None):
-        return False
-
-admin.site.register(Perfil, ReadOnlyPerfilAdmin)
+admin.site.register(Perfil)
