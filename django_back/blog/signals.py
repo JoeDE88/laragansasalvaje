1e38464f324104196da47f3a155c90ec78457853
JoEDE88 <joediaz88@hotmail.com>
2025-05-30 16:55:20 +0200

    added signals para eliminar img cloudinary con elemento

---
+++ django_back/blog/signals.py
@@ -0,0 +1,14 @@
+import cloudinary.uploader
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
+import cloudinary
+
+from .models import Publicacion
+
+@receiver(post_delete,sender=Publicacion)
+def eliminar_imagen_cloudinary(sender,instance, **kwargs):
+    if instance.imagen:
+        public_id = instance.imagen.public_id
+        if public_id:
+            cloudinary.uploader.destroy(public_id)
--- django_back/galeria/apps.py
+++ django_back/galeria/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class GaleriaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'galeria'
+
+def ready(self):
+    import galeria.signals
