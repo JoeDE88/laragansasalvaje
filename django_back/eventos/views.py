73ba8412c713295592c3287c701feb090725b36d
JoEDE88 <joediaz88@hotmail.com>
2025-06-05 13:17:10 +0200

    added create_event con jwt required

--- django_back/eventos/views.py
+++ django_back/eventos/views.py
@@ -1,26 +1,44 @@
 from django.shortcuts import render
 import json
+from django.views.decorators.http import require_GET,require_POST
+from django_back.utils import jwt_required, get_user_from_token
 from django.http import JsonResponse
 from .models import Evento
 
-#descomenta la siguiente linea si quieres hacer POST DE PRUEBA DESDE POSTMAN
-#from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 
-#descomenta la siguiente linea si quieres hacer POST DE PRUEBA DESDE POSTMAN
-#@csrf_exempt
+@require_GET
 def eventos_list(request):
-    if request.method == 'GET':
-        eventos = Evento.objects.all()
-        data = []
+    eventos = Evento.objects.all()
+    data = []
 
-        for evento in eventos:
-            data.append({
-                'id': evento.id,
-                'nombre': evento.nombre,
-                'descripcion': evento.descripcion,
-                'imagen': evento.imagen.url,
-                'creado_en': evento.creado_en,
-                'slug': evento.slug
-            })
-            
-        return JsonResponse(data, safe=False)
+    for evento in eventos:
+        data.append({
+            'id': evento.id,
+            'nombre': evento.nombre,
+            'descripcion': evento.descripcion,
+            'imagen': evento.imagen.url,
+            'creado_en': evento.creado_en,
+            'slug': evento.slug
+        })
+        
+    return JsonResponse(data, safe=False)
+    
+@csrf_exempt
+@jwt_required
+@require_POST
+def create_event(request):
+    nombre = request.POST.get('nombre')
+    descripcion = request.POST.get('descripcion')
+    imagen = request.FILES.get('imagen')
+
+    new_event = Evento.objects.create(
+        nombre=nombre,
+        descripcion=descripcion,
+        imagen=imagen
+    )
+
+    return JsonResponse({
+        'message': 'Evento creado',
+        'evento_id': new_event.id
+    }, status=201)
