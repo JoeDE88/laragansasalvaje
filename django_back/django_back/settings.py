0f660d5d46224ab3a96cdcf59122a6c80f180962
JoEDE88 <joediaz88@hotmail.com>
2025-06-05 13:00:34 +0200

    instalado jwt y creadas funciones para auth con token

--- django_back/django_back/settings.py
+++ django_back/django_back/settings.py
@@ -23,16 +23,13 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY","clave_por_defecto_para_dev")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+JWT_SECRET = SECRET_KEY
+JWT_ALGORITHM = 'HS256'
+JWT_EXP_DELTA_SECONDS = 3600
 
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['.localhost',
     '127.0.0.1',
@@ -41,7 +38,6 @@ ALLOWED_HOSTS = ['.localhost',
     ]
 
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -96,16 +92,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "django_back.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
     }
 
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -123,9 +113,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "Europe/Madrid"
@@ -133,10 +120,6 @@ TIME_ZONE = "Europe/Madrid"
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -150,8 +133,6 @@ if not DEBUG:
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
--- django_back/django_back/utils.py
+++ django_back/django_back/utils.py
@@ -1,4 +1,47 @@
+import jwt
+from datetime import datetime,timedelta
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from functools import wraps
 from PIL import Image
+
+def generate_jwt(user):
+    payload = {
+        'user_id': user.id,
+        'username': user.username,
+        'exp': datetime.utcnow() + timedelta(seconds=settings.JWT_EXP_DELTA_SECONDS),
+        'iat': datetime.utcnow()
+    }
+    token = jwt.encode(payload,settings.JWT_SECRET,algorithm=settings.JWT_ALGORITHM)
+
+    return token if isinstance(token,str) else token.decode('utf-8')
+
+def get_user_from_token(token):
+    try:
+        payload = jwt.decode(token, settings.JWT_SECRET, algorithms=[settings.JWT_ALGORITHM])
+
+        user = User.objects.get(id=payload['user_id'])
+        return User
+    
+    except (jwt.ExpiredSignatureError, jwt.InvalidTokenError, User.DoesNotExist):
+        return None
+    
+def jwt_required(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        auth_header = request.headers.get('Authorization')
+        if not auth_header or not auth_header.startswith('Bearer '):
+            return JsonResponse({'error':'Token no proporcionado'}, status=401)
+        
+        token = auth_header.split(' ')[1]
+        user = get_user_from_token(token)
+        if not user :
+            return JsonResponse({'error':'Token inválido o expirado'},status=401)
+        
+        request.user = user
+        return view_func(request, *args, **kwargs)
+    return wrapper
 
 def resize_image(image_path, max_width=640,max_height=480):
     img = Image.open(image_path)
