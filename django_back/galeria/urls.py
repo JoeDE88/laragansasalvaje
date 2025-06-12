351872544d1406fbd0ee66c85004f12a9f581f17
JoEDE88 <joediaz88@hotmail.com>
2025-06-12 11:56:52 +0200

    eliminada vista verificar storage

--- django_back/galeria/urls.py
+++ django_back/galeria/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 
-from .views import listado_obras, listado_obras_por_tema, crear_obra, primeras_obras_por_categoria,verificar_storage
+from .views import listado_obras, listado_obras_por_tema, crear_obra, primeras_obras_por_categoria
 
 urlpatterns = [
     path("obras/", listado_obras, name='listado_obras'),
     path("<slug:categoria_slug>", listado_obras_por_tema, name='obras_por_tema'),
     path("nueva/", crear_obra,name='crear_obra'),
-    path("primeras/", primeras_obras_por_categoria,name='primeras_obras'),
-    path("verificar-storage/", verificar_storage),
-
+    path("primeras/", primeras_obras_por_categoria,name='primeras_obras')
 ]
